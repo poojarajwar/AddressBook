844febbe1d2697c8b14108b87e2425c659fe11fa
Dell <Dell@DESKTOP-T21QER1>
2021-03-27 14:32:30 +0530

    First commit
    upload all file

---
+++ AdreessBook/src/com/bridgelabz/model/Address.java
@@ -0,0 +1,48 @@
+package com.bridgelabz.model;
+
+public class Address {
+	private String fullName;
+	private String address;
+	private String country;
+	private String city;
+	private String state;
+	private String zipCode;
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	
+	
+}
